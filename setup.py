687b4371253100ba137d90535d9750131c7ff5eb
Kris <mealskristofer@gmail.com>
2022-12-01 21:20:00 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DuckShooter',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Kris Meals',
+    author_email='',
+    description=''
+)
